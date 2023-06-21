1869ec5514430c106e3c0a3a5fa8101964d1b4a7
yoonewon <57882279+yoonewon@users.noreply.github.com>
2023-06-21 18:03:37 +0900

    Create exercise4.1.js
    exercise 4.1

---
+++ exercise4.1.js
@@ -0,0 +1,7 @@
+const setRandomBgColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const newColor = 'rgb(' + red + ',' + blue + ',' + green + ')';
+    document.getElementById('color-box').style.backgroundColor = newColor;
+};
